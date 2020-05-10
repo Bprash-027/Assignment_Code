5acaf3b004d2465a13ae4eda69c63a13544e66c6
Prashanth <pbhosle05@gmail.com>
2020-05-10 12:00:29 +0530

    Deleted some lines of code

---
+++ area_of_circle.py
@@ -0,0 +1,15 @@
+# Area of Circle
+# A=PI * R * R
+
+def area_of_cirle(rad):
+  area_of_cirle = 3.14 * rad * rad
+  print("Area of circle with radius " + str(rad) + " is " + str(area_of_cirle) + "cm")
+ 
+
+area_of_cirle(23.45)
+area_of_cirle(56.78)
+
+
+
+
+
